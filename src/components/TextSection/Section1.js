5c0d38c395557c71092b781c19047ce64c907c5b
Waheed Uddin Ahmed <dmc4719@gmail.com>
2023-04-01 06:17:56 +0600

    Update texts

--- src/components/TextSection/Section1.js
+++ src/components/TextSection/Section1.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { TypeAnimation } from "react-type-animation";
 
-const texts = ["a full stack software engineer", "a DIY enthusiast"];
 const variants = {
   enter: (direction) => {
     return {
@@ -25,18 +24,6 @@ const variants = {
 };
 
 const Section1 = () => {
-  const [index, setIndex] = useState(0);
-
-  useEffect(() => {
-    setTimeout(() => {
-      let next = index + 1;
-      if (next === texts.length) {
-        next = 0;
-      }
-      setIndex(next);
-    }, 3 * 1000);
-  }, [index, setIndex]);
-
   return (
     <div className="h-screen flex items-center justify-start">
       <motion.div className=" px-[20px]">
@@ -47,20 +34,12 @@ const Section1 = () => {
           repeat={Infinity}
         />
         <h1 className="text-5xl text-white mt-4">Melanie Rodrigues</h1>
-        <AnimatePresence>
-          <motion.span
-            style={{ position: "absolute" }}
-            variants={variants}
-            key={index}
-            initial="enter"
-            animate="center"
-            exit="exit"
-            className="text-2xl mt-4 text-white"
-            transition={{ duration: 0.5, ease: "easeOut" }}
-          >
-            {texts[index]}
-          </motion.span>
-        </AnimatePresence>
+        <span
+          style={{ position: "absolute" }}
+          className="text-2xl mt-4 text-white"
+        >
+          a full stack software engineer
+        </span>
       </motion.div>
     </div>
   );
